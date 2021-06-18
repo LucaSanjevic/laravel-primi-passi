c06642975b914382c4f5f1b5c1936232d8770d85
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-06-18 14:37:53 +0200

    fatto

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/numeri', function () {
+    return view('numeri', [
+      'telefono'=>'052276378463',
+      'cellulare'=>'3408396627'
+    ]
+  );
 });
